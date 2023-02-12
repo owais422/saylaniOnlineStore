c26338a3fd19e13b53b0b1d9415e1d2b880790c1
owais422 <106330677+owais422@users.noreply.github.com>
2023-02-12 21:37:28 +0500

    Add files via upload

---
+++ build/precache-manifest.53ad71d5a71d1704cf4d0a0fd9165d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf99b0e32ced8b7a607",
+    "url": "%20owais422.github.io/saylaniOnlineStore/static/css/main.bf40d383.chunk.css"
+  },
+  {
+    "revision": "fcf99b0e32ced8b7a607",
+    "url": "%20owais422.github.io/saylaniOnlineStore/static/js/main.fcf99b0e.chunk.js"
+  },
+  {
+    "revision": "c39b4e9b4978ee4f8400",
+    "url": "%20owais422.github.io/saylaniOnlineStore/static/js/1.c39b4e9b.chunk.js"
+  },
+  {
+    "revision": "5680ecbdeed868fcee40",
+    "url": "%20owais422.github.io/saylaniOnlineStore/static/js/2.5680ecbd.chunk.js"
+  },
+  {
+    "revision": "47a5a10096e692ce21b5",
+    "url": "%20owais422.github.io/saylaniOnlineStore/static/js/runtime~main.47a5a100.js"
+  },
+  {
+    "revision": "f9f1578476d788a205e049072ec22be5",
+    "url": "%20owais422.github.io/saylaniOnlineStore/index.html"
+  }
+];
